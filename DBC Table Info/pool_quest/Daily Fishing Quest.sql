dabe4686fc4812149838cad24b74507437f6e9e4
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-05-08 21:13:42 -0400

    Add files via upload

---
+++ DBC Table Info/pool_quest/Daily Fishing Quest.sql
@@ -0,0 +1,11 @@
+INSERT INTO pool_quest
+(entry, pool_entry, description)
+VALUES(90020, 80000, 'Deep Sea Fishing');
+
+INSERT INTO pool_quest
+(entry, pool_entry, description)
+VALUES(90021, 80000, 'Manta Ray');
+
+INSERT INTO pool_quest
+(entry, pool_entry, description)
+VALUES(90022, 80000, 'Moonglow Cuttlefish');
