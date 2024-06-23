93060853b1abace4e73940ae344713bb7ec85e56
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-06-23 09:44:50 -0400

    Update Commander Snarltooth Ironhide (101093).sql
    Fixed Smartscript error:  SmartAIMgr: Entry 101093 SourceType 0 Event 1 Action 1 using non-existent Text id 0, skipped.

--- Enemies/Commander Snarltooth Ironhide (101093).sql
+++ Enemies/Commander Snarltooth Ironhide (101093).sql
@@ -18,10 +18,11 @@ INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`,
 --SMART AI
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 101093;
 
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 101093;
+
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 101093);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (101093, 0, 0, 1, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 6268, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Commander Snarltooth Ironhide - On Aggro - Cast \'Rushing Charge\''),
-(101093, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Commander Snarltooth Ironhide - On Aggro - Say Line 0'),
 (101093, 0, 2, 0, 0, 0, 100, 0, 1300, 3700, 20000, 25000, 0, 0, 11, 6730, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Commander Snarltooth Ironhide - In Combat - Cast \'Head Butt\''),
 (101093, 0, 3, 0, 0, 0, 100, 0, 4800, 6000, 30000, 30000, 0, 0, 11, 6016, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Commander Snarltooth Ironhide - In Combat - Cast \'Pierce Armor\'');
 
