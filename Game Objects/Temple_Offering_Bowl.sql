3de3964389bcde3b73e3bd61e3d86669454aec6b
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-06-23 10:29:00 -0400

    Create Temple_Offering_Bowl.sql

---
+++ Game Objects/Temple_Offering_Bowl.sql
@@ -0,0 +1,9 @@
+--GameObject Info
+DELETE FROM `gameobject_template` WHERE (`entry` = 1000100);
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES
+(1000100, 5, 7695, 'Temple Offering Bowl', '', '', '', 1, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', -18019);
+
+--SPAWN
+DELETE FROM `gameobject` WHERE (`id` = 1000100);
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`, `Comment`) VALUES
+(8000885, 1000100, 5000, 0, 0, 1, 1, -14031.7, 3205.71, 121.42, 0.888084, 0, 0, -0.429593, -0.903023, 300, 0, 1, '', NULL, NULL);
