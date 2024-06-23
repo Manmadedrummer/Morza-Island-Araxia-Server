e2615523b56900e0868cd09baa15742d46baf72d
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-06-23 10:10:58 -0400

    Update Imposter Queen (800801).sql
    Fixed Smart Script

--- Enemies/Imposter Queen (800801).sql
+++ Enemies/Imposter Queen (800801).sql
@@ -12,3 +12,13 @@ INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`,
 DELETE FROM `creature` WHERE (`id1` = 800801);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (9000239, 800801, 0, 0, 5000, 0, 0, 1, 1, 1, -14129.2, 3080.06, 221.392, 3.12965, 300, 0, 0, 184230, 0, 0, 1, 0, 0, '', NULL);
+
+
+--Smart Script
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 800801;
+
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 800801);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(800801, 0, 0, 1, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 6268, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, '(Imposter) Queen Elaine - On Aggro - Cast \'Rushing Charge\''),
+(800801, 0, 2, 0, 0, 0, 100, 0, 1300, 3700, 20000, 25000, 0, 0, 11, 6730, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, '(Imposter) Queen Elaine - In Combat - Cast \'Head Butt\''),
+(800801, 0, 3, 0, 0, 0, 100, 0, 4800, 6000, 30000, 30000, 0, 0, 11, 6016, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, '(Imposter) Queen Elaine - In Combat - Cast \'Pierce Armor\'');
