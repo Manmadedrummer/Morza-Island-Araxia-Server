bfcf866f804b856599a61b4a61cb45509de599c5
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-06-23 09:57:16 -0400

    Update Imposter King (800800).sql
    Fixed Smart Script

--- Enemies/Imposter King (800800).sql
+++ Enemies/Imposter King (800800).sql
@@ -12,3 +12,13 @@ INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`,
 DELETE FROM `creature` WHERE (`id1` = 800800);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
 (9000240, 800800, 0, 0, 5000, 0, 0, 1, 1, 1, -14134.2, 3084.22, 223.725, 4.23918, 300, 0, 0, 164760, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
+
+--Smart Script
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 800800;
+
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 800800);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(800800, 0, 0, 0, 0, 0, 100, 0, 18700, 28500, 18700, 28500, 0, 0, 11, 6253, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, '(Imposter) King Theramis - In Combat - Cast \'Backhand\''),
+(800800, 0, 1, 0, 0, 0, 100, 0, 21100, 33900, 21100, 33900, 0, 0, 11, 19134, 1, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, '(Imposter) King Theramis - In Combat - Cast \'Frightening Shout\''),
+(800800, 0, 2, 0, 0, 0, 100, 0, 42800, 42800, 42800, 42800, 0, 0, 11, 23600, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, '(Imposter) King Theramis - In Combat - Cast \'Piercing Howl\''),
+(800800, 0, 3, 4, 2, 0, 100, 1, 0, 30, 0, 0, 0, 0, 11, 8599, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, '(Imposter) King Theramis - Between 0-30% Health - Cast \'Enrage\' (No Repeat)');
