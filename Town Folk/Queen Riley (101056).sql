d9105038bf96a38aa6839c2efa819c4b709f8a07
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-06-23 10:35:27 -0400

    Update Queen Riley (101056).sql
    Fixed DB error: Creature (Entry: 101056) has invalid unit_class (0) in creature_template. Set to 1 (UNIT_CLASS_WARRIOR).

--- Town Folk/Queen Riley (101056).sql
+++ Town Folk/Queen Riley (101056).sql
@@ -1,7 +1,7 @@
 --Creatures
 DELETE FROM `creature_template` WHERE (`entry` = 101056);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(101056, 0, 0, 0, 0, 0, 24965, 0, 0, 0, 'Queen Riley', 'Queen of Morza Island', NULL, 0, 80, 80, 0, 35, 3, 2.5, 2.5, 2.5, 2.5, 20, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 6000000, 6000000, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', NULL);
+(101056, 0, 0, 0, 0, 0, 24965, 0, 0, 0, 'Queen Riley', 'Queen of Morza Island', 'Speak', 0, 80, 80, 0, 35, 3, 2.5, 2.5, 2.5, 2.5, 20, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 6000000, 6000000, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', NULL);
 
 --SPAWN
 DELETE FROM `creature` WHERE (`id1` = 101056);
