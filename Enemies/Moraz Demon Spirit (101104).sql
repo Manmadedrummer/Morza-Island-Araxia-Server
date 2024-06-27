f0a9fec2e2840ade24a6470581255907e68514e5
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-06-26 22:02:22 -0400

    Update Moraz Demon Spirit (101104).sql

--- Enemies/Moraz Demon Spirit (101104).sql
+++ Enemies/Moraz Demon Spirit (101104).sql
@@ -1,7 +1,7 @@
 --Creature
 DELETE FROM `creature_template` WHERE (`entry` = 101104);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(101104, 0, 0, 0, 0, 0, 4427, 0, 0, 0, 'Moraz Demon Spirit', NULL, NULL, 0, 80, 80, 0, 90, 0, 1, 1.14286, 1, 1, 20, 1, 1, 0, 7.5, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 3, 0, 11876, 0, 0, 0, 0, 0, 0, 'SmartAI', 0, 1, 40, 15, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
+(101104, 0, 0, 0, 0, 0, 4427, 0, 0, 0, 'Morza Demon Spirit', NULL, NULL, 0, 80, 80, 0, 90, 0, 1, 1.14286, 1, 1, 20, 1, 1, 0, 7.5, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 3, 0, 11876, 0, 0, 0, 0, 0, 0, 'SmartAI', 0, 1, 40, 15, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
 
 --SMART AI
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 101104;
