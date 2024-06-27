3b9d4a6be650eec4681f617944af648e050fd1ed
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-06-26 22:00:16 -0400

    Update and rename Moraz Lasher (101098).sql to Morza Lasher (101098).sql

--- Enemies/Morza Lasher (101098).sql
+++ Enemies/Morza Lasher (101098).sql
@@ -1,7 +1,7 @@
 --Creature
 DELETE FROM `creature_template` WHERE (`entry` = 101098);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(101098, 0, 0, 0, 0, 0, 19006, 0, 0, 0, 'Moraz Lasher', '', NULL, 0, 70, 80, 0, 7, 0, 1.2, 1.14286, 1, 1, 20, 1, 1, 0, 10, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 4, 0, 800000, 0, 0, 0, 0, 100, 15000, '', 0, 1, 10, 1, 1, 1, 0, 100, 1, 0, 0, 0, '', 12340);
+(101098, 0, 0, 0, 0, 0, 19006, 0, 0, 0, 'Morza Lasher', '', NULL, 0, 70, 80, 0, 7, 0, 1.2, 1.14286, 1, 1, 20, 1, 1, 0, 10, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 4, 0, 800000, 0, 0, 0, 0, 100, 15000, '', 0, 1, 10, 1, 1, 1, 0, 100, 1, 0, 0, 0, '', 12340);
 
 --Creature Loot
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 800000);
