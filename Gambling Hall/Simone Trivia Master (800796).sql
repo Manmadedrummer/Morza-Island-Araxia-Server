c5edafda942aa4f29f4f4c128dfbefdbd70c6fde
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-05 08:40:58 -0400

    Create Simone Trivia Master (800796).sql

---
+++ Gambling Hall/Simone Trivia Master (800796).sql
@@ -0,0 +1,11 @@
+--Creature_Template
+
+DELETE FROM `creature_template` WHERE (`entry` = 800796);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(800796, 0, 0, 0, 0, 0, 25041, 0, 0, 0, 'Simone', 'Trivia Master', NULL, 0, 80, 80, 0, 35, 1, 1, 1.14286, 1, 1, 18, 1, 1, 0, 1, 2000, 2000, 1, 1, 1, 768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+
+
+--Spawn
+DELETE FROM `creature` WHERE (`id1` = 800796);
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(9000659, 800796, 0, 0, 5000, 0, 0, 1, 1, 0, -13790.3, 2931.78, 0.47218, 3.11965, 300, 0, 0, 5876, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
