c8bb8dd7e487d6b4eedb7e123053c9cc39d6bed1
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-06 14:48:26 -0400

    Update Toki Wartooth (101108).sql

--- Concert/Toki Wartooth (101108).sql
+++ Concert/Toki Wartooth (101108).sql
@@ -1,7 +1,7 @@
 --Creature Tempalte
 DELETE FROM `creature_template` WHERE (`entry` = 101108);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(101108, 0, 0, 0, 0, 0, 24379, 0, 0, 0, 'Toki Wartooth', '', NULL, 0, 70, 75, 1, 35, 2, 1, 1.14286, 1, 1, 20, 1, 2, 0, 1, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+(101108, 0, 0, 0, 0, 0, 24379, 0, 0, 0, 'Toki Wartooth', 'Dethklok Guitarist', NULL, 0, 70, 75, 1, 35, 2, 1, 1.14286, 1, 1, 20, 1, 2, 0, 1, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
 
 --SPAWN
 DELETE FROM `creature` WHERE (`id1` = 101108);
