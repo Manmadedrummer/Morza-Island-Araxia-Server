75db2a6112398e42aea15604fe73bcda5742dd90
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-17 16:21:03 -0400

    Update Cow Bandit (101106).sql

--- Enemies/Cow Bandit (101106).sql
+++ Enemies/Cow Bandit (101106).sql
@@ -1,7 +1,7 @@
 --Creature Template
 DELETE FROM `creature_template` WHERE (`entry` = 101106);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(101106, 0, 0, 0, 0, 0, 5568, 5569, 0, 0, 'Cow Bandit', NULL, NULL, 0, 76, 80, 0, 16, 0, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 5618, 5618, 0, 0, 0, 60, 380, 'SmartAI', 1, 1, 1.2, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
+(101106, 0, 0, 0, 0, 0, 5568, 5569, 0, 0, 'Cow Bandit', NULL, NULL, 0, 76, 80, 0, 168, 0, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 5618, 5618, 0, 0, 0, 60, 380, 'SmartAI', 1, 1, 1.2, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
 
 
 --SMART SCRIPT
