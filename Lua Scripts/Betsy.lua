0f922d7b5e5755cff1494396796b2536ff438a86
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-17 16:18:25 -0400

    Update Betsy.lua

--- Lua Scripts/Betsy.lua
+++ Lua Scripts/Betsy.lua
@@ -1,4 +1,4 @@
---Made by Manmadedrummer for the Araxia Server
+--Betsy Escort Made by Manmadedrummer for the Araxia Server
 
 local WAYPOINTS = {
     { -13864.030, 3296.922, 60.913, 3.61104 },
@@ -15,93 +15,109 @@ local BOSS_ID = 101107 -- ID of the bandit boss
 
 local npcWaypoints = {}
 
-local function MoveToNextWaypoint(creature, wpIndex)
+local function MoveToNextWaypoint(eventId, delay, repeats, creature)
+    local guid = creature:GetGUIDLow()
+    local wpIndex = npcWaypoints[guid]
+
+    if not wpIndex then
+        wpIndex = 1
+        npcWaypoints[guid] = wpIndex
+    end
+
     local waypoint = WAYPOINTS[wpIndex]
-    creature:MoveTo(wpIndex, waypoint[1], waypoint[2], waypoint[3], waypoint[4])
+    if waypoint then
+        creature:MoveTo(wpIndex, waypoint[1], waypoint[2], waypoint[3], waypoint[4])
+    else
+        npcWaypoints[guid] = nil
+    end
 end
 
-local function OnReachWaypoint(event, creature, type, id)
+local function OnReachWaypoint(eventId, delay, repeats, creature)
     local guid = creature:GetGUIDLow()
     local wpIndex = npcWaypoints[guid]
 
     if wpIndex == 1 then
         creature:RemoveEvents()
         creature:SendUnitSay("Moo!! Moo!!", 0)
+        local bandit1 = creature:SpawnCreature(BANDIT_ID, WAYPOINTS[1][1] + 2, WAYPOINTS[1][2] + 2, WAYPOINTS[1][3], WAYPOINTS[1][4], 5, 60000)
+        local bandit2 = creature:SpawnCreature(BANDIT_ID, WAYPOINTS[1][1] - 2, WAYPOINTS[1][2] - 2, WAYPOINTS[1][3], WAYPOINTS[1][4], 5, 60000)
+        if bandit1 then 
+            bandit1:SendUnitYell("Get the cow!", 0)
+            bandit1:AttackStart(creature) 
+        end
+        if bandit2 then 
+            bandit2:SendUnitYell("There she is!", 0)
+            bandit2:AttackStart(creature) 
+        end
 
-        -- Spawn the bandits and make them attack Betsy
-        local bandit1 = creature:SpawnCreature(BANDIT_ID, WAYPOINTS[1][1] + 2, WAYPOINTS[1][2] + 2, WAYPOINTS[1][3], WAYPOINTS[1][4], 5, 60000) -- Offset position
-        local bandit2 = creature:SpawnCreature(BANDIT_ID, WAYPOINTS[1][1] - 2, WAYPOINTS[1][2] - 2, WAYPOINTS[1][3], WAYPOINTS[1][4], 5, 60000) -- Offset position
-
-        if bandit1 then bandit1:AttackStart(creature) end
-        if bandit2 then bandit2:AttackStart(creature) end
-
-        -- Continue moving to the next waypoint after 5 seconds
-        creature:RegisterEvent(function(event, delay, repeats)
-            MoveToNextWaypoint(creature, 2)
+        creature:RegisterEvent(function(eventId, delay, repeats, c)
             npcWaypoints[guid] = 2
+            MoveToNextWaypoint(eventId, delay, repeats, c)
         end, 5000, 1)
 
     elseif wpIndex == 2 then
         creature:RemoveEvents()
-        -- Wait for 5 seconds before moving to the next waypoint
-        creature:RegisterEvent(function(event, delay, repeats)
-            MoveToNextWaypoint(creature, 3)
+        creature:RegisterEvent(function(eventId, delay, repeats, c)
             npcWaypoints[guid] = 3
+            MoveToNextWaypoint(eventId, delay, repeats, c)
         end, 5000, 1)
 
     elseif wpIndex == 3 then
         creature:RemoveEvents()
         creature:SendUnitSay("Moo!!", 0)
+        local bandit = creature:SpawnCreature(BANDIT_ID, WAYPOINTS[3][1] + 2, WAYPOINTS[3][2] + 2, WAYPOINTS[3][3], WAYPOINTS[3][4], 5, 60000)
+        if bandit then 
+            bandit:SendUnitYell("You made an udder mistake.", 0)
+            bandit:AttackStart(creature) 
+        end
 
-        -- Spawn another bandit and make it attack Betsy
-        local bandit = creature:SpawnCreature(BANDIT_ID, WAYPOINTS[3][1] + 2, WAYPOINTS[3][2] - 2, WAYPOINTS[3][3], WAYPOINTS[3][4], 5, 60000) -- Offset position
-        if bandit then bandit:AttackStart(creature) end
-
-        -- Wait for 10 seconds before moving to the next waypoint
-        creature:RegisterEvent(function(event, delay, repeats)
-            MoveToNextWaypoint(creature, 4)
+        creature:RegisterEvent(function(eventId, delay, repeats, c)
             npcWaypoints[guid] = 4
+            MoveToNextWaypoint(eventId, delay, repeats, c)
         end, 10000, 1)
 
     elseif wpIndex == 4 then
         creature:RemoveEvents()
         creature:SendUnitSay("Moo!!", 0)
+        local boss = creature:SpawnCreature(BOSS_ID, WAYPOINTS[4][1] + 2, WAYPOINTS[4][2] + 2, WAYPOINTS[4][3], WAYPOINTS[4][4], 5, 60000)
+        if boss then 
+            boss:SendUnitYell("Foolish cow, you're mine!", 0)
+            boss:AttackStart(creature) 
+        end
 
-        -- Spawn the bandit boss and make it attack Betsy
-        local boss = creature:SpawnCreature(BOSS_ID, WAYPOINTS[4][1] + 2, WAYPOINTS[4][2] + 2, WAYPOINTS[4][3], WAYPOINTS[4][4], 5, 60000) -- Offset position
-        if boss then boss:AttackStart(creature) end
-
-        -- Wait for 20 seconds before moving to the next waypoint
-        creature:RegisterEvent(function(event, delay, repeats)
-            MoveToNextWaypoint(creature, 5)
+        creature:RegisterEvent(function(eventId, delay, repeats, c)
             npcWaypoints[guid] = 5
+            MoveToNextWaypoint(eventId, delay, repeats, c)
         end, 20000, 1)
 
     elseif wpIndex == 5 then
         creature:RemoveEvents()
         creature:SendUnitSay("Moooooooooooooooo", 0)
         npcWaypoints[guid] = nil
-        
     else
         wpIndex = wpIndex + 1
         npcWaypoints[guid] = wpIndex
-        MoveToNextWaypoint(creature, wpIndex)
+        MoveToNextWaypoint(nil, nil, nil, creature)
     end
+end
+
+local function HandleWaypoint(eventId, creature, type, id)
+    OnReachWaypoint(nil, nil, nil, creature)
 end
 
 local function StartEscort(creature)
     local guid = creature:GetGUIDLow()
     npcWaypoints[guid] = 1
-    MoveToNextWaypoint(creature, 1)
+    MoveToNextWaypoint(nil, nil, nil, creature)
     creature:RegisterEvent(OnReachWaypoint, 1000, 0)
 end
 
 local function OnQuestAccept(event, player, creature, quest)
     if quest:GetId() == QUEST_ID then
-        creature:SendUnitSay("Moo", 0)
+        creature:SendUnitSay("Moo ma moo", 0)
         StartEscort(creature)
     end
 end
 
 RegisterCreatureEvent(NPC_ID, 31, OnQuestAccept)
-RegisterCreatureEvent(NPC_ID, 6, OnReachWaypoint)
+RegisterCreatureEvent(NPC_ID, 6, HandleWaypoint)
