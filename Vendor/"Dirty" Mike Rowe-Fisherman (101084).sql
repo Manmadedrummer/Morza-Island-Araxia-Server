f9e35b13ed6d322e6a86ceb8610409cfb56046b1
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-19 09:11:36 -0400

    Update and rename Dirt Mike Rowe-Fissherman (101084).sql to "Dirty" Mike Rowe-Fisherman (101084).sql

--- Vendor/"Dirty" Mike Rowe-Fisherman (101084).sql
+++ Vendor/"Dirty" Mike Rowe-Fisherman (101084).sql
@@ -1,7 +1,7 @@
 --Creature
 DELETE FROM `creature_template` WHERE (`entry` = 101084);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(101084, 0, 0, 0, 0, 0, 21891, 0, 0, 0, '"Dirty" Michael Crowe', 'Fish Merchant', NULL, 8826, 36, 36, 0, 894, 643, 1, 1.14286, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 32768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'SmartAI', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
+(101084, 0, 0, 0, 0, 0, 21891, 0, 0, 0, '"Dirty" Michael Crowe', 'Fish Merchant', NULL, 8826, 36, 36, 0, 35, 643, 1, 1.14286, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 32768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'SmartAI', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
 
 --VENDOR ITEMS
 DELETE FROM `npc_vendor` WHERE (`entry` = 101084);
