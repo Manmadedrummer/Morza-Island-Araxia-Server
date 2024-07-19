9fa41020d8c36eeaacb8549e70d2eee295ac85e5
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-19 08:30:19 -0400

    Update Morza Guard (101024).sql

--- Town Folk/Morza Guard (101024).sql
+++ Town Folk/Morza Guard (101024).sql
@@ -1,7 +1,7 @@
 --Creature
 DELETE FROM `creature_template` WHERE (`entry` = 101024);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(101024, 0, 0, 0, 0, 0, 3705, 3708, 25021, 25065, 'Morza Guard', NULL, NULL, 0, 45, 45, 0, 35, 0, 1, 1.42857, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 2386, 2386, 0, 0, 0, 0, 0, 'SmartAI', 0, 1, 1.25, 1, 1, 1, 0, 144, 1, 0, 0, 0, '', 12340);
+(101024, 0, 0, 0, 0, 0, 3705, 3708, 25021, 25065, 'Morza Guard', NULL, NULL, 0, 45, 45, 0, 35, 1, 1, 1.42857, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 2386, 2386, 0, 0, 0, 0, 0, 'GuardAI', 0, 1, 1.25, 1, 1, 1, 0, 144, 1, 0, 0, 0, '', 12340);
 
 --SPAWN
 DELETE FROM `creature` WHERE (`id1` = 101024);
