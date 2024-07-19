f01b306d142b76d6486750e7be745290ed4c40a5
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-19 10:57:30 -0400

    Create Grum The Holy-Paladin-Trainer (26327).sql

---
+++ Grum The Holy-Paladin-Trainer (26327).sql
@@ -0,0 +1,15 @@
+--CREATURE TEMPLATE
+DELETE FROM `creature_template` WHERE (`entry` = 26327);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(26327, 0, 0, 0, 0, 0, 24032, 0, 0, 0, 'Grum The Holy', 'Paladin Trainer', '', 0, 70, 70, 2, 35, 48, 1, 1.14286, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 2, 768, 2048, 0, 0, 0, 0, 2, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
+
+--NPC TRAINER
+DELETE FROM `npc_trainer` WHERE (`ID` = 26327);
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
+(26327, -200004, 0, 0, 0, 0, 0),
+(26327, -200003, 0, 0, 0, 0, 0);
+
+--SPAWN
+DELETE FROM `creature` WHERE (`id1` = 26327);
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(9000132, 26327, 0, 0, 5000, 0, 0, 1, 1, 0, -13760.7, 3229.24, 12.6305, 6.1566, 300, 0, 0, 11227, 3155, 0, 0, 0, 0, '', NULL, 0, NULL);
