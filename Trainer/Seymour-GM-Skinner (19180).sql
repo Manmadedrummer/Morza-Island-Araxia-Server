db004edcdfefbb2bd346e5791410edfcefbd7f3d
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-19 09:50:20 -0400

    Create Seymour-GM-Skinner (19180).sql

---
+++ Trainer/Seymour-GM-Skinner (19180).sql
@@ -0,0 +1,16 @@
+--Creature Template
+DELETE FROM `creature_template` WHERE (`entry` = 19180);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(19180, 0, 0, 0, 0, 0, 18623, 0, 0, 0, 'Seymour', 'Grand Master Skinner', NULL, 7818, 60, 65, 1, 35, 81, 1, 1.14286, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 33024, 2048, 0, 0, 2, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 1.05, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+
+--NPC Trainer
+DELETE FROM `npc_trainer` WHERE (`ID` = 19180);
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
+(19180, -201038, 0, 0, 0, 0, 0),
+(19180, -201037, 0, 0, 0, 0, 0),
+(19180, -201036, 0, 0, 0, 0, 0);
+
+--SPAWN
+DELETE FROM `creature` WHERE (`id1` = 19180);
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(9000124, 19180, 0, 0, 5000, 0, 0, 1, 1, 1, -13733.3, 3225.38, 14.1048, 0.721093, 300, 0, 0, 6770, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
